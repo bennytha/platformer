1e744d15d905caaac3102f75a007a7756e7d47a5
Benny T <thazhuthabenny@gmail.com>
2026-07-03 12:22:15 +0530

    added input manager

--- common/scenes/hud.gd
+++ common/scenes/hud.gd
@@ -1,4 +1,6 @@
 extends Control
+@onready var touch_screen_ui: Control = $TouchScreenUI
+@onready var keyboard_ui: Control = $KeyboardUI
 
 @onready var bag: Control = $Bag
 
@@ -13,5 +15,19 @@ func _ready() -> void:
 	else:
 		print('could not find local_bus')
 		
+	InputManager.device_changed.connect(_on_device_changed)
+	update_hint(InputManager.current_device)
+
 func toggle_bag(value:bool):
 	bag.visible = value
+
+func _on_device_changed(new_device: InputManager.DeviceType) -> void:
+	update_hint(new_device)
+
+func update_hint(device: InputManager.DeviceType) -> void:
+	if device == InputManager.DeviceType.TOUCH:
+		touch_screen_ui.visible = true
+		keyboard_ui.visible = false
+	else:
+		touch_screen_ui.visible = false
+		keyboard_ui.visible = true
