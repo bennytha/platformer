5d646497bfb3a66221cd29e0cf08da951f61cfba
Benny T <thazhuthabenny@gmail.com>
2026-07-18 20:55:04 +0530

    pause menu focus fixed

--- game/screens/hud_ui/pause_menu/pause_menu.gd
+++ game/screens/hud_ui/pause_menu/pause_menu.gd
@@ -5,20 +5,26 @@ extends Control
 @export_file("*.tscn") var game_container_path: String = "res://game/screens/game_conatiner/game_container.tscn"
 
 @onready var close: Button = $PanelContainer/VBoxContainer/HBoxContainer2/Close
-@onready var retry: Button = $PanelContainer/VBoxContainer/HBoxContainer/Retry
 
 var local_bus: GameContainer
 
 func _ready() -> void:
 	local_bus = UtilsFuncs.find_local_bus(self)
-	retry.grab_focus()
-	
+	visibility_changed.connect(_on_visibility_changed)
+	if is_visible_in_tree():
+		close.grab_focus()
+
 func _on_close_pressed() -> void:
 	if local_bus:
 		local_bus.show_menu.emit(false)
 	else:
 		print('local bus not found')
 
+func _on_visibility_changed() -> void:
+	# Only grab focus when the menu actually becomes visible in the tree
+	if is_visible_in_tree():
+		# Using call_deferred ensures Godot sets focus after the UI layout updates
+		close.grab_focus.call_deferred()
 
 func _on_home_pressed() -> void:
 	SceneChanger.change_scene(start_screen_path)
