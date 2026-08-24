5f67cb433dbebfc56d8fe79551850b7ca25d3af3
Benny T <thazhuthabenny@gmail.com>
2026-08-24 20:36:15 +0530

    discord link added

--- game/screens/start_screen/start_screen.gd
+++ game/screens/start_screen/start_screen.gd
@@ -1,6 +1,14 @@
 extends Node2D
 @onready var player: CharacterBody2D = $Player
 
+const DISCORD_URL:String = 'https://discord.gg/44PNjJsJM'
+
 func _ready() -> void:
 	player.set_camera_enabled(false)
 	player.set_player_input_enabled(false)
+
+
+func _on_discord_pressed() -> void:
+	var error = OS.shell_open(DISCORD_URL)
+	if error != OK:
+		push_error("Failed to open URL: ", error)
